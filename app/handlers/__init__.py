5732cddff08b2bd230e89f1395d0ddf29e8220f5
yusrafaheem <yusra.faheem@gwmail.gwu.edu>
2026-08-08 19:48:54 -0400

    Add handlers package init

---
+++ app/handlers/__init__.py
@@ -0,0 +1,4 @@
+"""Tornado RequestHandler subclasses, one module per resource area. All of
+them inherit from base.BaseHandler for JSON responses, auth, and running
+blocking work off the event loop.
+"""
